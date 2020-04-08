ab608fd0c0fc02d82676112fcb60435e0b198ffe
banane <47035692+bananeee@users.noreply.github.com>
2020-04-08 22:27:07 +0700

    dropzone term file

--- SceneChild1.js
+++ SceneChild1.js
@@ -11,14 +11,15 @@ class SceneChild1 extends Phaser.Scene {
     }
 
     create() {
-        
+
         this.stop = false;                              // stop all animation when dragging
 
         this.packageStacked = [];                       // package wait to be displayed
         this.packageOnTrack = [];                       // package is being displayed
-        
+
+        this.setUpDropzone();
         this.packageSetup();
-        
+
         this.inputManager();
 
         // Repeatedly put package to the screen after a duration
@@ -30,7 +31,7 @@ class SceneChild1 extends Phaser.Scene {
         this.text3 = this.add.text(32, 72, { fill: '#32a852' });
         this.text100 = this.add.text(32, 92, { fill: '#32a852' });
     }
- 
+
     update() {
         if (this.packageOnTrack.length != 0 && this.stop == false) {
             this.move(2);
@@ -48,6 +49,23 @@ class SceneChild1 extends Phaser.Scene {
         ]);
     }
 
+    // Drop zone 
+    setUpDropzone() {
+        this.zoneWeekDay = this.add.zone(0.23 * config.width, 0.78 * config.height).setRectangleDropZone(0.46 * config.width, 0.46 * config.height);
+        this.zoneHoliay = this.add.zone(0.77 * config.width, 0.78 * config.height).setRectangleDropZone(0.46 * config.width, 0.46 * config.height);
+
+        var graphic1 = this.add.graphics();
+        graphic1.lineStyle(2, 0xffff00);
+        graphic1.strokeRect(this.zoneWeekDay.x - this.zoneWeekDay.input.hitArea.width / 2,
+            this.zoneWeekDay.y - this.zoneWeekDay.input.hitArea.height / 2,
+            this.zoneWeekDay.input.hitArea.width, this.zoneWeekDay.input.hitArea.height);
+        var graphic2 = this.add.graphics();
+        graphic2.lineStyle(2, 0xffff00);
+        graphic2.strokeRect(this.zoneHoliay.x - this.zoneHoliay.input.hitArea.width / 2,
+            this.zoneHoliay.y - this.zoneHoliay.input.hitArea.height / 2,
+            this.zoneHoliay.input.hitArea.width, this.zoneHoliay.input.hitArea.height);
+    }
+
     // Random the order of package and set up 
     packageSetup() {
         for (let i = 0; i < 3; i++) {
@@ -61,7 +79,7 @@ class SceneChild1 extends Phaser.Scene {
         this.input.dragDistanceThreshold = 16;
 
         this.input.on('dragstart', function (pointer, gameObject) {
-            //this.children.bringToTop(gameObject);
+            this.children.bringToTop(gameObject);
             // gameObject.setTint(0xff0000);
             this.stop = true;
         }, this);
@@ -77,11 +95,37 @@ class SceneChild1 extends Phaser.Scene {
             gameObject.y = dragY;
         });
 
+        this.input.on('drop', function (pointer, gameObject, dropZone) {
 
-        this.input.on('dragend', function(pointer, gameObject) {
-            this.stop = false;
+            gameObject.x = dropZone.x;
+            gameObject.y = dropZone.y;
+
+            gameObject.input.enabled = false;
+
+        });
+
+        this.input.on('dragend', function (pointer, gameObject, dropped) {
+
             gameObject.clearTint();
+            if (!dropped) {
+                
+                var tween = this.tweens.add({
+                    targets: gameObject,
+                    x: gameObject.input.dragStartX,
+                    y: gameObject.input.dragStartY,
+                    // delay: 1000,
+                    duration: 1000,
+                    ease: 'Linear'
+                });
+                // gameObject.x = gameObject.input.dragStartX;
+                // gameObject.y = gameObject.input.dragStartY;
+
+            }
+            var timedEvent = this.time.delayedCall(1000, function () {
+                this.stop = false;
+            }, [], this);
         }, this);
+
     }
 
     // Package moves on track
@@ -100,7 +144,7 @@ class SceneChild1 extends Phaser.Scene {
         }
     }
 
-    
+
     onEvent() {
         if (this.packageStacked.length > 0)
             this.packageOnTrack.push(this.packageStacked.shift());
