70da560d7950da1c257d2585d1dcc1ed93fbf860
banane <47035692+bananeee@users.noreply.github.com>
2020-04-24 01:46:25 +0700

    add hover effect to SceneRoot

--- SceneRoot.js
+++ SceneRoot.js
@@ -9,21 +9,37 @@ class SceneRoot extends Phaser.Scene {
     }
 
     create() {
+        this.textSetup();
+        this.buttonImageSetup();
+
+    }
+
+    buttonImageSetup() {
         // Choose lesson
-        this.add.image(0.325 * config.width,0.4 * config.height, 'button1').setInteractive().on('pointerdown', function(){
+        this.add.image(0.325 * config.width, 0.4 * config.height, 'button1').setInteractive().on('pointerdown', function () {
             console.log('change scene');
-            this.scene.start('Lesson1');           
+            this.scene.start('Lesson1');
+        }, this).on('pointerover', function () {
+            this.morningDetail.setColor('#ff0000');
+        }, this).on('pointerout', function () {
+            this.morningDetail.setColor('#000000');
         }, this);
 
-        this.add.image(0.675 * config.width,0.4 * config.height, 'button2').setInteractive().on('pointerdown', function(){
+        this.add.image(0.675 * config.width, 0.4 * config.height, 'button2').setInteractive().on('pointerdown', function () {
             console.log('change scene');
-            this.scene.start('Choose');           
-        }, this);
-        
+            this.scene.start('Choose');
+        }, this).on('pointerover', function () {
+            this.afternoonDetail.setColor('#ff0000');
+        }, this).on('pointerout', function () {
+            this.afternoonDetail.setColor('#000000');
+        }, this);;
+    }
+
+    textSetup() {
         // Lesson detail
         this.morningDetail = this.make.text({
-            x: 0.325*config.width,
-            y: 0.7*config.height,
+            x: 0.325 * config.width,
+            y: 0.7 * config.height,
             text: 'Weekdays and weekends',
             origin: { x: 0.5, y: 0.5 },
             style: {
@@ -34,8 +50,8 @@ class SceneRoot extends Phaser.Scene {
             }
         });
         this.afternoonDetail = this.make.text({
-            x: 0.675*config.width,
-            y: 0.7*config.height,
+            x: 0.675 * config.width,
+            y: 0.7 * config.height,
             text: 'A day in the life of a primary school learner',
             origin: { x: 0.5, y: 0.5 },
             style: {
@@ -45,12 +61,6 @@ class SceneRoot extends Phaser.Scene {
                 wordWrap: { width: 300 }
             }
         });
-        
 
     }
-    
-    update(){
-
-    }
-
 }
